464ca17077e5e82656a2569ab537fc2fc5d67040
Jueshi <1528344561@qq.com>
2023-12-03 16:09:16 +0800

    mock

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,9 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-          target:'http://localhost:8080',//后台服务
+          //后台服务.想用Mock Server就改这个url 
+          target:'http://localhost:8080',
+          // target:'https://038dad50-a444-4552-9e12-d1de96224b4b.mock.pstmn.io',
           changeOrigin:true,
           rewrite:(path)=>path.replace(/^\/api/,'')// /api 替换为空串
       }
