368d7e88dd8270857e8e9e8c3b9f1b22ad2b1493
echoqsm <1277559651@qq.com>
2023-12-25 12:39:36 +0800

    bar

--- src/views/bar.vue
+++ src/views/bar.vue
@@ -82,84 +82,87 @@ onMounted(() => {
 <template>
     <!-- {{ this.$route.query }}
     {{ this.$route.query.barName }} -->
-    <div class="barInfo">
-        <div class="barAvatar_bord">
-        <div class="barAvatar" >
-            <img :src="bar.barPic">
-        </div>
-        </div>
-
-            <div class="barProfile">
-                <div class="barName">
-                    <span >{{ bar.barName }}吧</span>
-                </div>
-            <div class="barIntroduction">
-                <span >{{ bar.barIntroduction }}</span>
+    <div class="head_main">
+        <div class="barInfo">
+            <div class="barAvatar_bord">
+            <div class="barAvatar" >
+                <img :src="bar.barPic">
+            </div>
             </div>
 
-            <div class="barMoreInfo">
-                <div class="number_label">
-                    <span>贴子:</span>
+                <div class="barProfile">
+                    <div class="barName">
+                        <span >{{ bar.barName }}吧</span>
+                    </div>
+                    
+                <div class="barMoreInfo">
+                    <div class="number_label">
+                        <span>贴子数量:</span>
+                    </div>
+                    <div class="number_num">
+                        <span>{{ posts.length }}</span>
+                    </div>
                 </div>
-                <div class="number_num">
-                    <span>{{ posts.length }}</span>
+                <div class="barIntroduction">
+                    <span >{{ bar.barIntroduction }}</span>
                 </div>
+
             </div>
         </div>
-    </div>
 
 
 
+        <div class="forum_rcmd">
+            <div class="barShow">
+            <el-main class="page-container">
 
+            <el-table :data="posts" style="width: 100%" show-header="false" @row-click="handleRowClick">
+            
+                <el-table-column
+                    prop="postTitle"
+                    label=""
+                    width="250">
+            
+            <!-- <div>
+                <img :src="bars.barPic">
+                <p>{{ bars.barName }}</p>
 
+            </div> -->
+            <template #default="{ row }">
+                <div class="row-container">
+            
 
-    <div class="forum_rcmd">
-        <div class="barShow">
-        <el-main class="page-container">
-
-        <el-table :data="posts" style="width: 100%" show-header="false" @row-click="handleRowClick">
-        
-            <el-table-column
-                prop="postTitle"
-                label=""
-                width="250">
-        
-        <!-- <div>
-            <img :src="bars.barPic">
-            <p>{{ bars.barName }}</p>
-
-        </div> -->
-        <template #default="{ row }">
-            <div class="row-container">
-        
-
-        <div class="post">
-            <!-- <img :src="row.barPic" alt="Bar Image" width="60" height="60" object-fit="fill"> -->
-        </div>
-        <div class="postTitle">
+            <div class="post">
+                <!-- <img :src="row.barPic" alt="Bar Image" width="60" height="60" object-fit="fill"> -->
+            </div>
             <div class="postTitle">
-                {{ row.postTitle }}
-            </div>
-            <div class="barPeopleNum">
+                <div class="postTitle">
+                    {{ row.postTitle }}
+                </div>
+                <div class="barPeopleNum">
 
+                </div>
             </div>
         </div>
-      </div>
-    </template>
-      </el-table-column>
-      <el-table-column
-        prop="postContent"
-        label=""
-        width="180">
-      </el-table-column>
-    </el-table>
-        </el-main>
+        </template>
+        </el-table-column>
+        <el-table-column
+            prop="postContent"
+            label=""
+            width="180">
+        </el-table-column>
+        </el-table>
+            </el-main>
+            </div>
         </div>
     </div>
-
 </template>
 
 <style lang="scss" scoped>
+.head_main{
+    position: relative;
+    background-color: #dceffe;
+}
 .barAvatar_bord{
     padding: 2px;
     border: solid 1px rgba(0,0,0,.1);
@@ -168,36 +171,53 @@ onMounted(() => {
     top: 0;
     margin: 10px;
 }
-.barAvatar{
-    padding: 4px;
-    overflow: hidden;
-    background: #FFF;
-    z-index: 9;
-    top: auto;
-    bottom: -5px;
-    border-color: #DADCDF;
-    margin: 20px;
+.barInfo[data-v-b013e4e0] {
+    border: 2px solid wheat;
+    width: 900px;
+    height: 300px;
+    padding: 10px;
+    position: relative;
+    left: 300px;
+    background-color: white;
 }
-.barInfo{
-    display: flex;
-    flex-direction: row; 
-    align-items: center;
+.barAvatar_bord[data-v-b013e4e0] {
+    padding: 2px;
+    border: solid 1px rgba(0, 0, 0, 0.1);
+    background: #fff;
+    margin: 10px;
+    width: 120px;
+    position: relative;
+    top: 110px;
 }
+
+.barProfile[data-v-b013e4e0][data-v-b013e4e0] {
+    justify-content: flex-start;
+    margin-right: 10px;
+    position: relative;
+    left: 200px;
+    top: -30px;
+}
+
 .barName{
     font-size: 22px;
 }
+//贴吧数量
 .barMoreInfo{
     font-size: 12px;
 }
 .number_label {
     color: #AAA;
+    display: inline-block;
 }
 .number_num{
     color: #ff7f3e;
     font-family: Arial;
+    display: inline-block;
 }
 .barIntroduction{
     color: #4c4c4c;
+    width: 100px;
+    height: 100px;
     font-size: 14px;
     float: left;
     padding-right: 30px;
@@ -206,16 +226,16 @@ onMounted(() => {
     white-space: nowrap;
     text-overflow: ellipsis;
     overflow: hidden;
+    // overflow: scroll;
 }
-.barProfile{
-    display: flex;
-    justify-content: flex-start;
-    margin-left: 10px;
-    margin-right: 0;
-}
-.barName{
-    font-weight: 10px;
-    margin-right: 0;
-    margin-top: 3px;
+.forum_rcmd {
+    border: 2px solid white;
+    width: 922px;
+    background: #fff;
+    box-sizing: border-box;
+    font-family: STHeiti,"Microsoft Yahei","Microsoft YaHei",Arial,sans-serif;
+    margin: 0 auto;
+    position: relative;
+    left: 15px;
 }
 </style>
